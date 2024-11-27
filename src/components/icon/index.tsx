5d7e275a5e6f29065f69955c8d53e17b9246afd9
RakaDoank <rakaaudira@gmail.com>
2024-11-27 17:27:45 +0700

    move backup icon svg string outside of React

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -30,11 +30,6 @@ export function Icon({
 			/>
 		)
 	} catch(e) {
-		/**
-		 * Copied from  
-		 * https://github.com/carbon-design-system/carbon-react-native/blob/main/src/helpers/index.tsx
-		 */
-		const backupIcon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13.17 13.69"><line x1="0.71" y1="0.97" x2="12.46" y2="12.72" fill="currentColor" stroke="currentColor" stroke-miterlimit="10" stroke-width="2"/><line x1="1.02" y1="13.02" x2="12.15" y2="0.67" fill="currentColor" stroke="currentColor" stroke-miterlimit="10" stroke-width="2"/></svg>'
 
 		return (
 			<SvgXml
@@ -47,3 +42,9 @@ export function Icon({
 	}
 
 }
+
+/**
+ * Copied from  
+ * https://github.com/carbon-design-system/carbon-react-native/blob/main/src/helpers/index.tsx
+ */
+const backupIcon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13.17 13.69"><line x1="0.71" y1="0.97" x2="12.46" y2="12.72" fill="currentColor" stroke="currentColor" stroke-miterlimit="10" stroke-width="2"/><line x1="1.02" y1="13.02" x2="12.15" y2="0.67" fill="currentColor" stroke="currentColor" stroke-miterlimit="10" stroke-width="2"/></svg>'
