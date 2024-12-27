226f0da285e64b0649fa7583ce270d3540777217
RakaDoank <rakaaudira@gmail.com>
2024-12-28 00:02:32 +0700

    Update Button docs

--- package/src/components/button/base-color/index.tsx
+++ package/src/components/button/base-color/index.tsx
@@ -41,7 +41,13 @@ export interface BaseColorProps extends Omit<BaseProps, 'backgroundNode' | 'icon
 		text: Record<BaseColorState, TextStyle>,
 		icon: Record<BaseColorState, string>,
 	},
+	/**
+	 * It extends `iconNode` prop from `BaseProps` to add one argument at start for icon coloring
+	 */
 	iconNode?: (
+		/**
+		 * Use this param to coloring an icon correctly according each variant of button and its state, like focused, disabled, etc.
+		 */
 		iconColorState: string,
 		...params: Parameters<NonNullable<BaseProps['iconNode']>>
 	) => React.ReactNode,
--- package/src/components/button/base/index.tsx
+++ package/src/components/button/base/index.tsx
@@ -37,16 +37,26 @@ import type {
 } from '../size'
 
 export interface BaseProps extends Omit<PressableProps, 'children' | 'style'> {
+	/**
+	 * Refer to https://carbondesignsystem.com/components/button/style/#sizes  
+	 * Default is `large_productive`
+	 */
 	size?: ButtonSize,
 	text?: string,
 	icon?: SharedType.CarbonIcon,
 	iconColor?: string,
 	/**
 	 * This prop is useful to custom render at the icon position.  
-	 * `iconNode` takes precedence even if `icon` prop is present.
+	 * `iconNode` takes precedence even if {@linkcode icon} prop is present.
 	 */
 	iconNode?: (
+		/**
+		 * Use this param to resize icon view correctly according to each size of button
+		 */
 		iconSize: number,
+		/**
+		 * Use this param to keep icon style nicely center aligned vertically with text
+		 */
 		iconStyle: IconProps['style'],
 	) => React.ReactNode,
 	/**
