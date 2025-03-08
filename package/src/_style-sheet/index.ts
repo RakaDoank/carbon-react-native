60b12ace02d16b019943f8163d8dc129bcc7471d
RakaDoank <rakaaudira@gmail.com>
2025-03-09 02:09:48 +0700

    add absoluteFill

--- package/src/_style-sheet/index.ts
+++ package/src/_style-sheet/index.ts
@@ -1,9 +1,15 @@
 import {
+	StyleSheet as RNStyleSheet,
+} from 'react-native'
+
+import {
 	create as _create,
 } from './create'
 
 export namespace StyleSheet {
 
 	export const create = _create
+
+	export const absoluteFill = RNStyleSheet.absoluteFill
 
 }
