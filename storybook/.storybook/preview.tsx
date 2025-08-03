ea5e387ed1327a8cbab59142331d93c323cdc345
RakaDoank <rakaaudira@gmail.com>
2025-08-03 18:04:51 +0700

    use background context of storybook to carbon

--- storybook/.storybook/preview.tsx
+++ storybook/.storybook/preview.tsx
@@ -6,19 +6,41 @@ import {
 	CarbonReactNative,
 } from '@audira/carbon-react-native'
 
+import {
+	Color,
+} from '@audira/carbon-react-native-elements'
+
+import './body.css'
 import './font.css'
 
 export default {
 
 	decorators: [
-		Story => (
-			<CarbonReactNative colorScheme="gray_10">
-				<Story/>
-			</CarbonReactNative>
-		),
+		(Story, context) => {
+			return (
+				<CarbonReactNative
+					// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+					colorScheme={ context.globals.backgrounds.value === 'dark' ? 'gray_100' : 'gray_10' }
+				>
+					<Story/>
+				</CarbonReactNative>
+			)
+		},
 	],
 
 	parameters: {
+		backgrounds: {
+			options: {
+				dark: {
+					name: 'gray-100',
+					value: Color.Token.gray_100.background,
+				},
+				light: {
+					name: 'gray-10',
+					value: Color.Token.gray_10.background,
+				},
+			},
+		},
 		controls: {
 			matchers: {
 				color: /(background|color)$/i,
