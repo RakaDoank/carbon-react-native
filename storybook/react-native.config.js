824c61e33ff3aaddeb8a0ef38b01c7f9e3208398
RakaDoank <rakaaudira@gmail.com>
2025-08-10 23:56:45 +0700

    fix: it's already autolinked

--- storybook/react-native.config.js
+++ storybook/react-native.config.js
@@ -1,55 +1,16 @@
 const
 	node_path =
-		require('node:path'),
-
-	packageJson =
-		require('./package.json')
+		require('node:path')
 
 const
 	workspaceRoot =
-		node_path.join(__dirname, '..'),
-
-	repositoryNodeModules =
-		node_path.join(workspaceRoot, 'node_modules'),
-
-	rnDependencies =
-		Object.keys(packageJson.dependencies),
-
-	ignoreRnDeps =
-		[
-			'react',
-			'react-native',
-		]
+		node_path.join(__dirname, '..')
 
 module.exports = {
 
 	assets: [
 		node_path.join(workspaceRoot, 'assets/fonts'),
 	],
-
-	/**
-	 * Since this repository is a monorepo setup with hoisted node_modules
-	 * We have to manually put the React Native libraries to the correct node_modules path
-	 */
-	dependencies: Object.assign(
-		{
-			// Put your other dependencies to be linked
-		},
-		rnDependencies.reduce((
-			/**
-			 * @type {Record<string, string>}
-			 */
-			acc,
-			dependency,
-		) => {
-			if(ignoreRnDeps.indexOf(dependency) == -1) {
-				acc[dependency] = {
-					root: node_path.join(repositoryNodeModules, dependency),
-				}
-			}
-			return acc
-		}, {}),
-	),
 
 	project: {
 		ios: {
