94ebf1b5e997bd4f76f5381efd25341c92a75476
RakaDoank <rakaaudira@gmail.com>
2025-10-12 20:24:32 +0700

    update docs

--- storybook/types/carbon-icons.d.ts
+++ storybook/types/carbon-icons.d.ts
@@ -1,1 +1,8 @@
-declare module '@carbon/icons*'
+declare module '@carbon/icons/svg/*.svg' {
+	import type {
+		SvgProps,
+	} from 'react-native-svg'
+
+	const Component: React.FunctionComponent<SvgProps>
+	export default Component
+}
