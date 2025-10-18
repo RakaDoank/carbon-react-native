97864db2c5b49a88eb6b82e53164d6b3d83efab9
RakaDoank <rakaaudira@gmail.com>
2025-10-18 20:29:18 +0700

    update docs

--- storybook/.storybook/manager.ts
+++ storybook/.storybook/manager.ts
@@ -2,5 +2,11 @@ import * as ManagerApi from 'storybook/manager-api'
 import * as Theming from 'storybook/theming'
 
 ManagerApi.addons.setConfig({
-	theme: Theming.themes.light,
+	theme: {
+		...Theming.themes.light,
+		brandTitle: '@audira/carbon-react-native',
+		fontBase: `"IBMPlexSans-Regular", system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif`,
+		barBg: '#161616',
+		barTextColor: '#ffffff',
+	},
 })
