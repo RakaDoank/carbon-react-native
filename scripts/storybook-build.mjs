d6f5d1552f8d852d7f62640fa07493a142249b18
RakaDoank <rakaaudira@gmail.com>
2026-03-14 01:09:01 +0700

    Build carbon-react-native first to generate necessary `CarbonStyleSheet.g` members

--- scripts/storybook-build.mjs
+++ scripts/storybook-build.mjs
@@ -7,6 +7,14 @@ const
 		node_path.join(import.meta.dirname, "..")
 
 node_childProcess.execSync(
+	"npm run packages-builder:carbon-react-native",
+	{
+		cwd: root,
+		stdio: "inherit",
+	},
+)
+
+node_childProcess.execSync(
 	"npx storybook build --disable-telemetry --output-dir .dist",
 	{
 		cwd: node_path.join(root, "storybook"),
