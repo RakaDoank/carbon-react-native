2b624a5a46e64a4b9158bc8ed104844a972d1d95
RakaDoank <rakaaudira@gmail.com>
2026-03-14 02:08:54 +0700

    Fix wrong type

--- packages/carbon-react-native/src/carbon-style-sheet/g-object/text.ts
+++ packages/carbon-react-native/src/carbon-style-sheet/g-object/text.ts
@@ -1,43 +1,43 @@
 import type {
-	ViewStyle,
+	TextStyle,
 } from "react-native"
 
 export const text_center = {
 	textAlign: "center",
-} as const satisfies ViewStyle
+} as const satisfies TextStyle
 
 export const text_left = {
 	textAlign: "left",
-} as const satisfies ViewStyle
+} as const satisfies TextStyle
 
 export const text_right = {
 	textAlign: "right",
-} as const satisfies ViewStyle
+} as const satisfies TextStyle
 
 export const underline = {
 	textDecorationLine: "underline",
-} as const satisfies ViewStyle
+} as const satisfies TextStyle
 
 export const line_through = {
 	textDecorationLine: "line-through",
-} as const satisfies ViewStyle
+} as const satisfies TextStyle
 
 export const no_underline = {
 	textDecorationLine: "none",
-} as const satisfies ViewStyle
+} as const satisfies TextStyle
 
 export const align_auto = {
 	verticalAlign: "auto",
-} as const satisfies ViewStyle
+} as const satisfies TextStyle
 
 export const align_top = {
 	verticalAlign: "top",
-} as const satisfies ViewStyle
+} as const satisfies TextStyle
 
 export const align_middle = {
 	verticalAlign: "middle",
-} as const satisfies ViewStyle
+} as const satisfies TextStyle
 
 export const align_bottom = {
 	verticalAlign: "bottom",
-} as const satisfies ViewStyle
+} as const satisfies TextStyle
--- packages/carbon-react-native/src/carbon-style-sheet/g/text.ts
+++ packages/carbon-react-native/src/carbon-style-sheet/g/text.ts
@@ -5,59 +5,59 @@ import {
 export const { text_center } = StyleSheet.create({
 	text_center: {
 		textAlign: "center",
-	},
+	} as const,
 })
 
 export const { text_left } = StyleSheet.create({
 	text_left: {
 		textAlign: "left",
-	},
+	} as const,
 })
 
 export const { text_right } = StyleSheet.create({
 	text_right: {
 		textAlign: "right",
-	},
+	} as const,
 })
 
 export const { underline } = StyleSheet.create({
 	underline: {
 		textDecorationLine: "underline",
-	},
+	} as const,
 })
 
 export const { line_through } = StyleSheet.create({
 	line_through: {
 		textDecorationLine: "line-through",
-	},
+	} as const,
 })
 
 export const { no_underline } = StyleSheet.create({
 	no_underline: {
 		textDecorationLine: "none",
-	},
+	} as const,
 })
 
 export const { align_auto } = StyleSheet.create({
 	align_auto: {
 		verticalAlign: "auto",
-	},
+	} as const,
 })
 
 export const { align_top } = StyleSheet.create({
 	align_top: {
 		verticalAlign: "top",
-	},
+	} as const,
 })
 
 export const { align_middle } = StyleSheet.create({
 	align_middle: {
 		verticalAlign: "middle",
-	},
+	} as const,
 })
 
 export const { align_bottom } = StyleSheet.create({
 	align_bottom: {
 		verticalAlign: "bottom",
-	},
+	} as const,
 })
