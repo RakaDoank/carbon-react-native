18ce8d62153c8f012214717f010c69271dc59cb6
RakaDoank <rakaaudira@gmail.com>
2026-07-23 16:59:22 +0700

    add width style by breakpoint

--- packages/carbon-react-native/src/carbon-style-sheet/g-object/width.ts
+++ packages/carbon-react-native/src/carbon-style-sheet/g-object/width.ts
@@ -2,6 +2,10 @@ import type {
 	ViewStyle,
 } from "react-native"
 
+import {
+	Breakpoint,
+} from "@audira/carbon-react-native-elements"
+
 export const w_auto = {
 	width: "auto",
 } as const satisfies ViewStyle
@@ -11,9 +15,51 @@ export const w_full = {
 } as const satisfies ViewStyle
 
 export const max_w_auto = {
-	width: "auto",
+	maxWidth: "auto",
 } as const satisfies ViewStyle
 
 export const max_w_full = {
-	width: "100%",
+	maxWidth: "100%",
 } as const satisfies ViewStyle
+
+// +++ Element Breakpoint +++
+export const w_small = {
+	width: Breakpoint.small.value.px,
+} as const satisfies ViewStyle
+
+export const w_medium = {
+	width: Breakpoint.medium.value.px,
+} as const satisfies ViewStyle
+
+export const w_large = {
+	width: Breakpoint.large.value.px,
+} as const satisfies ViewStyle
+
+export const w_x_large = {
+	width: Breakpoint.x_large.value.px,
+} as const satisfies ViewStyle
+
+export const w_max = {
+	width: Breakpoint.max.value.px,
+} as const satisfies ViewStyle
+
+export const max_w_small = {
+	maxWidth: Breakpoint.small.value.px,
+} as const satisfies ViewStyle
+
+export const max_w_medium = {
+	maxWidth: Breakpoint.medium.value.px,
+} as const satisfies ViewStyle
+
+export const max_w_large = {
+	maxWidth: Breakpoint.large.value.px,
+} as const satisfies ViewStyle
+
+export const max_w_x_large = {
+	maxWidth: Breakpoint.x_large.value.px,
+} as const satisfies ViewStyle
+
+export const max_w_max = {
+	maxWidth: Breakpoint.max.value.px,
+} as const satisfies ViewStyle
+// --- Element Breakpoint ---
--- packages/carbon-react-native/src/carbon-style-sheet/g/width.ts
+++ packages/carbon-react-native/src/carbon-style-sheet/g/width.ts
@@ -2,6 +2,10 @@ import {
 	StyleSheet,
 } from "react-native"
 
+import {
+	Breakpoint,
+} from "@audira/carbon-react-native-elements"
+
 export const { w_auto } = StyleSheet.create({
 	w_auto: {
 		width: "auto",
@@ -25,3 +29,65 @@ export const { max_w_full } = StyleSheet.create({
 		maxWidth: "100%",
 	} as const,
 })
+
+// +++ Element Breakpoint +++
+export const { w_small } = StyleSheet.create({
+	w_small: {
+		width: Breakpoint.small.value.px,
+	} as const,
+})
+
+export const { w_medium } = StyleSheet.create({
+	w_medium: {
+		width: Breakpoint.medium.value.px,
+	} as const,
+})
+
+export const { w_large } = StyleSheet.create({
+	w_large: {
+		width: Breakpoint.large.value.px,
+	} as const,
+})
+
+export const { w_x_large } = StyleSheet.create({
+	w_x_large: {
+		width: Breakpoint.x_large.value.px,
+	} as const,
+})
+
+export const { w_max } = StyleSheet.create({
+	w_max: {
+		width: Breakpoint.max.value.px,
+	} as const,
+})
+
+export const { max_w_small } = StyleSheet.create({
+	max_w_small: {
+		maxWidth: Breakpoint.small.value.px,
+	} as const,
+})
+
+export const { max_w_medium } = StyleSheet.create({
+	max_w_medium: {
+		maxWidth: Breakpoint.medium.value.px,
+	} as const,
+})
+
+export const { max_w_large } = StyleSheet.create({
+	max_w_large: {
+		maxWidth: Breakpoint.large.value.px,
+	} as const,
+})
+
+export const { max_w_x_large } = StyleSheet.create({
+	max_w_x_large: {
+		maxWidth: Breakpoint.x_large.value.px,
+	} as const,
+})
+
+export const { max_w_max } = StyleSheet.create({
+	max_w_max: {
+		maxWidth: Breakpoint.max.value.px,
+	} as const,
+})
+// --- Element Breakpoint ---
